d1ecc3fbcb0496bd2dd8b3fca7e83368059c82d8
Felix Abele <felix.abele@googlemail.com>
2013-07-17 15:42:32 +0200

    Rails 3.2 Compatible and for BrowserCMS' CMS::User model

--- lib/simple_audit/simple_audit.rb
+++ lib/simple_audit/simple_audit.rb
@@ -46,8 +46,10 @@ module SimpleAudit
       def simple_audit(options = {}, &block)
         class_eval do
 
-          write_inheritable_attribute :username_method, (options[:username_method] || :name).to_sym
-          class_inheritable_reader :username_method
+          class_attribute :username_method
+          class_attribute :audit_changes
+
+          self.username_method = (options[:username_method] || :name).to_sym
 
           attributes_and_associations = proc do |record|
             changes = record.attributes
@@ -57,24 +59,36 @@ module SimpleAudit
             changes
           end
           audit_changes_proc = block_given? ? block.to_proc : attributes_and_associations
-          write_inheritable_attribute :audit_changes, audit_changes_proc
-          class_inheritable_reader :audit_changes
+          
+          self.audit_changes = audit_changes_proc
 
           has_many :audits, :as => :auditable, :class_name => '::SimpleAudit::Audit'
 
           after_create {|record| record.class.audit(record, :create)}
           after_update {|record| record.class.audit(record, :update)}
-
         end
       end
 
       def audit(record, action = :update, user = nil) #:nodoc:
-        user ||= User.current if User.respond_to?(:current)
-        record.audits.create(:user => user,
-          :username => user.try(self.username_method),
-          :action => action.to_s,
-          :change_log => self.audit_changes.call(record)
-        )
+        
+        # check current user for typical Authentication systems
+        user = nil
+        if defined?(User) and User.respond_to?(:current)
+          user = User.current 
+        elsif defined?(Cms::User) and Cms::User.respond_to?(:current)
+          user = Cms::User.current 
+        end
+
+        if user.present?
+          record.audits.create(:user => user,
+            :username => user.try(self.username_method),
+            :action => action.to_s,
+            :change_log => self.audit_changes.call(record))
+        else
+          record.audits.create(
+            :action => action.to_s,
+            :change_log => self.audit_changes.call(record))
+        end
       end
     end
     
